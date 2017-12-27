8c4d4f3b952b600034e2a623909d189a24f23e8e
royanc <34865394+royanc@users.noreply.github.com>
2017-12-27 20:07:19 +0200

    Update UDP_sender.c
    עכשיו הוא שולח גם מולטי קאסט, כדאי לשנות את השליחה שתיהיה יותר יעילה בממקום לשלוח בית בית

--- UDP/UDP_sender.c
+++ UDP/UDP_sender.c
@@ -4,18 +4,34 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-//                                          compiling - gcc -g UDP_S.o -o UDP_S
+#include <string.h>
+
+//                  SENDER                        compiling - gcc -g UDP_S.o -o UDP_S
 #define MCAST_IP "239.0.0.1"
 #define PORT 6000
 struct in_addr localInterface;
 struct sockaddr_in groupSock;
 int sd;
-char databuf[1024] = "Multicast test message lol!";
+char databuf[1024];
 int datalen = sizeof(databuf);
 int ttl =100;
+int Num_of_Frame = 100000;
+FILE *fd;
+
+
 
 int main (int argc, char *argv[ ])
 {
+ 
+/*---- open file to sand: ----*/
+fd = fopen("alice.txt", "r");
+ if(fd ==NULL){
+	 perror("file open error");
+	 exit(1);
+ }
+ else
+	 printf("file open succeed");
+	
 /* Create a datagram socket on which to send. */
 sd = socket(AF_INET, SOCK_DGRAM, 0);
 if(sd < 0){
@@ -51,11 +67,35 @@ else
 /* Send a message to the multicast group specified by the*/
 /* groupSock sockaddr structure. */
 /*int datalen = 1024;*/
-
+/*
 if(sendto(sd, databuf, datalen, 0, (struct sockaddr*)&groupSock, sizeof(groupSock)) < 0)
 	perror("Sending datagram message error");
 else
 	printf("Sending datagram message...OK\n");
+
+*/
+
+
+  /*---- Send message to the socket of the incoming connection ----*/
+  while(Num_of_Frame)
+  {
+	  for(i=0;i<FRAME_SIZE;i++)
+	  	 {
+	  	 databuf[i] = fgetc(fd);
+	  	 if( feof(fd) ) {
+	  		Num_of_Frame = 0;
+	  	          break ;
+	  	       }
+	  	 }
+
+	  for(i=0;i<FRAME_SIZE;i++)
+	  	DEBUG("%c",buffer[i]);
+
+  //strcpy(buffer,"Hello World\n");
+   sendto(sd, databuf, datalen, 0, (struct sockaddr*)&groupSock, sizeof(groupSock));
+   printf("send \n");
+   Num_of_Frame--;
+  }
 
 //Finishing
 close(sd);
